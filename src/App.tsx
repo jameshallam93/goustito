b8c826c5824783dc577b2436fb7e169342eb8b89
James <james.hallam93@gmail.com>
2021-06-09 12:22:05 +0100

    Added basic search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const App = () => {
+const App: React.FunctionComponent = () => {
+
+  const [searchTerm, setSearchTerm] = useState("")
+  const [test, setTest] = useState("")
+
+  const handleSearch = (event: React.FormEvent) => {
+    event.preventDefault()
+    console.log(searchTerm)
+    if (searchTerm !== null) {
+      setTest(searchTerm)
+    }
+    return;
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault()
+    setSearchTerm(event.target.value)
+
+  }
   return (
     <div className="App">
       <h1>Goustito</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(event) => handleChange(event)}
+        />
+        <input
+          type="submit"
+        />
+      </form>
+      <p>{test}</p>
     </div>
   );
-}
+};
 
 export default App;
