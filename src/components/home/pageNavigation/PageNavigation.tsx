5b20cdc125d90fa274a536aa87c892142e91ecac
James <james.hallam93@gmail.com>
2021-06-10 16:48:32 +0100

    Moved all home components to home folder

---
+++ src/components/home/pageNavigation/PageNavigation.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { SHOW_NEXT_PAGE, SHOW_PREVIOUS_PAGE } from "../../../redux/actions";
+import { RecipeState } from "../../../redux/reducer";
+
+import "./pageNavigation.scss";
+const PageNavigation: React.FunctionComponent = () => {
+
+	const dispatch = useDispatch();
+	const startOfList = useSelector<RecipeState, boolean>((state) => state.resultsShown.from === 0);
+	const endOfList = useSelector<RecipeState, boolean>((state) =>
+		(state.resultsShown.to % 100 === 99)
+	);
+	const recipesAreLoaded = useSelector<RecipeState, boolean>((state) => state.recipes[0]?.length > 0);
+	const changePage = (direction: string): void => {
+		if (direction === "next") {
+			dispatch(SHOW_NEXT_PAGE());
+		}
+		if (direction === "previous") {
+			dispatch(SHOW_PREVIOUS_PAGE());
+		}
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		});
+	};
+	//todo - move this logic to scss
+	const showPrevious = { display: startOfList ? "none" : "" };
+	const showNext = { display: endOfList ? "none" : recipesAreLoaded ? "" : "none" };
+
+	return (
+		<div className="page-navigation">
+			<button style={showNext} onClick={() => changePage("next")}>Next</button>
+			<button style={showPrevious} onClick={() => changePage("previous")}>Previous</button>
+		</div>
+	);
+};
+export { PageNavigation };
---
+++ src/components/home/recipeList/Recipe.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+import "./recipe.scss";
+
+interface RecipeProps {
+	label: string,
+	url: string,
+	img: string,
+	calories: number,
+	source: string,
+	ingredients: string[],
+	servings: number
+}
+
+const Recipe: React.FunctionComponent<RecipeProps> = ({ label, url, img, calories, source, ingredients, servings }) => {
+	const [isHidden, setIsHidden] = useState<boolean>(true);
+
+	const handleExpand = () => {
+		setIsHidden(!isHidden);
+	};
+
+	//todo - move this logic to scss file - change handleExpand to alter className of hidden div
+	const hiddenStyle = { display: isHidden ? "none" : "" };
+
+	return (
+		<section
+			className="recipe"
+			onClick={handleExpand}
+		>
+			<h1>{label}</h1>
+			<div className="hidden" style={hiddenStyle}>
+				<ul className="ingredient-list">
+					{
+						ingredients.map(ingredient => {
+							return (<li key={ingredient}>{ingredient}</li>);
+						})
+					}
+				</ul>
+				<p>Calories: {Math.floor(calories)}</p>
+				<p>Servings: {servings} </p>
+			</div>
+
+			<img src={img} alt="food"></img>
+			<div></div>
+			<a href={url}>{source}</a>
+
+
+		</section>
+	);
+};
+
+export { Recipe };
