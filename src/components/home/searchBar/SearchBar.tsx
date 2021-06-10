b5cd767c8ae17a3e704f6ac61faf1a48978c43ec
James <james.hallam93@gmail.com>
2021-06-10 18:22:07 +0100

    Made home page responsive

--- src/components/home/searchBar/SearchBar.tsx
+++ src/components/home/searchBar/SearchBar.tsx
@@ -40,6 +40,7 @@ const SearchBar: React.FunctionComponent = () => {
 		<section className="search-bar">
 			<h3>Search through over 2.3 million recipes from all over the web!</h3>
 			<p>Click on a recipe card to see the ingredients and calories!</p>
+			<p>Click the link on the recipe card to visit the recipe page!</p>
 			<form onSubmit={handleSearch}>
 				<input
 					type={searchTerms.type}
