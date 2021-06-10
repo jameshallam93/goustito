0302ef944362fedfcfc3bada940f4e04b7f22dc3
James <james.hallam93@gmail.com>
2021-06-10 11:33:24 +0100

    Tests for request generator/minor refactor

--- src/axios/generateRequest.ts
+++ src/axios/generateRequest.ts
@@ -14,12 +14,22 @@ const generateMealTypeString = (mealTypes: string[]): string => {
     return mealTypeString;
 }
 
+const generateSearchTermString = (searchTerms: string) => {
+    const split = searchTerms.split(" ");
+    if (split.length === 1) {
+        return split[0];
+    }
+    const rejoined = split.join("%20");
+    return rejoined;
+}
+
 export const generateRequest = (searchTerms: string, mealTypes: string[]): string => {
+    const searchTermsString = generateSearchTermString(searchTerms);
     const mealTypeString = generateMealTypeString(mealTypes)
 
     let requestString = ``;
 
-    requestString = `${baseUrl}?q=${searchTerms}${apiVariables}`;
+    requestString = `${baseUrl}?q=${searchTermsString}${apiVariables}`;
 
     if (mealTypeString) {
         requestString = `${requestString}&mealType=${mealTypeString}`;
