9573a9b8b5133f1abb8357154319fe0badc99990
James <james.hallam93@gmail.com>
2021-06-11 12:41:34 +0100

    Refactored page navigation

---
+++ src/components/home/pageNavigation/changePage.ts
@@ -0,0 +1,14 @@
+import { SHOW_NEXT_PAGE, SHOW_PREVIOUS_PAGE } from "../../../redux/actions";
+
+export const changePage = (direction: string, dispatch: React.Dispatch<any>): void => { //eslint-disable-line
+	if (direction === "next") {
+		dispatch(SHOW_NEXT_PAGE());
+	}
+	if (direction === "previous") {
+		dispatch(SHOW_PREVIOUS_PAGE());
+	}
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	});
+};
