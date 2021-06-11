f6c5c9383bc7760a5bccc6b8b01cedf244e03e3c
James <james.hallam93@gmail.com>
2021-06-11 12:41:19 +0100

    Refactored checkbox array

---
+++ src/components/checkbox/CheckboxArray.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Checkbox } from "./Checkbox";
+
+interface CheckboxArrayProps {
+	categories: string[],
+	handleCheckboxChange: (label: string) => void,
+}
+
+const CheckboxArray: React.FunctionComponent<CheckboxArrayProps> = ({ categories, handleCheckboxChange }) => {
+	return (
+		<div className="checkbox-array">
+			{
+				categories.map(category => {
+					return (
+						<Checkbox
+							label={category}
+							handleCheckboxChange={handleCheckboxChange}
+							key={category}
+						/>
+					);
+				})
+			}
+		</div>
+	);
+};
+
+export { CheckboxArray };
