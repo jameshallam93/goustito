84b1735c2106cc52efb443161431a5ebd0624f8e
James <james.hallam93@gmail.com>
2021-06-11 15:40:23 +0100

    Styled loading component, added gif

--- src/components/pageElements/loading/Loading.tsx
+++ src/components/pageElements/loading/Loading.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RecipeState } from "../../../redux/reducer";
 
+import loadingIcon from "../../../assets/loading-icon.gif";
+
+import "./loading.scss";
+
 const Loading: React.FunctionComponent = () => {
 	const isLoading = useSelector<RecipeState, boolean>(state => state.isLoading);
 
 	if (isLoading) {
 		return (
 			<section className="loading">
-				<h1>loading</h1>
+				<h1>loading...</h1>
+				<img src={loadingIcon} alt="loading"></img>
 			</section>
 		);
 	}
