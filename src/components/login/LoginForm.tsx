e47aae1eaa32e1cc045a48646ba26198ee68c6c0
James <james.hallam93@gmail.com>
2021-06-12 11:42:14 +0100

    linked notification into login/signup forms

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 import { UserDetailsForm } from "./UserDetailsForm/UserDetailsForm";
+import { Notification, MessageType } from "../pageElements/notification/Notification";
+import { useNotification } from "../../hooks/useNotification";
 
 const LoginForm: React.FunctionComponent = () => {
 
+	const [notification, setNotification] = useNotification();
+
 	const handleLogin = (event: React.FormEvent<HTMLFormElement>, username: string, password: string) => {
+		event.preventDefault();
+		setNotification({ type: MessageType.error, message: "Test2" });
 		console.log(username, password);
 		return;
 	};
 	return (
 		<div className="login-form">
+			<Notification notification={notification} />
 			<UserDetailsForm
 				heading="Login"
 				handleSubmit={handleLogin}
